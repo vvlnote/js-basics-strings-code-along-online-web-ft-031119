3a02f3ba269e866cc1f1807b7e400de01367f237
Joy Lin <vvlnote@hotmail.com>
2019-06-14 20:56:03 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 let currentUser = "Joy";
 let welcomeMessage = "Welcome to Flatbook," + ' ' + currentUser + "!";
 let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + "!";
-const firstInitial = currentUSer[0];
-const restOfName = currentUser.slice(1);
-let shortGreeting = 'Welcome, ' + firstInitial + '!';
+
+let shortGreeting = 'Welcome, ' + currentUser[0] + '!';
