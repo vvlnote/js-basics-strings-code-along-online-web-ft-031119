ca8c23067b790ac43719bd3d1df0422f6d5971b2
Joy Lin <vvlnote@hotmail.com>
2019-06-14 21:03:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ let currentUser = "Joy";
 let welcomeMessage = "Welcome to Flatbook," + ' ' + currentUser + "!";
 let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + "!";
 
-let shortGreeting = 'Welcome, ' + currentUser[0] + '!';
+//let shortGreeting = 'Welcome, ' + currentUser[0] + '!';
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
